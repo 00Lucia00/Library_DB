3128ad6352afd6810dd5a6edf30c1a37f3421ef9
00Lucia00 <114389235+00Lucia00@users.noreply.github.com>
2023-01-17 21:12:59 +0100

    Addpage Design
    the adding a book page have been uppdated with new design

--- AddBook.page.xaml.cs
+++ AddBook.page.xaml.cs
@@ -24,5 +24,10 @@ namespace Library_DB
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
